63c2ab361a883645ade1b6bf292336ee527a28d1
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-11 15:16:25 -0500

    AAA pattern

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -1,9 +1,36 @@
 import { it, expect } from "vitest";
 import { add } from "./math";
 
+// AAA pattern for usnit testing
 it("should summerize all numbers in an Array : ", () => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  const numbers = [12, 2, 3, 8, 6]; //Arrange phase
+  const result = add(numbers); //Act phase
+  //Assert Phase
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => prevValue + curValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
 });
 
+it("should send NaN if a least one invalid number is provided", () => {
+  // AAA pattern -> Arrange , Act, Assert
+  const inputs = ["invalid", 1];
 
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct NaN if an array of numeric string value is provided", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => +prevValue + +curValue,
+    0
+  );
+
+  expect(result).toBe(expectedResult);
+});
